991525816e68d94bce91e209b5870eeac44838c6
Zach Olson <zo103181@gmail.com>
2023-04-07 21:32:25 -0500

    update tailwindcss and improve showcase project

--- projects/hero-icons-showcase/src/app/app.component.ts
+++ projects/hero-icons-showcase/src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { heroIconArray } from 'projects/hero-icons/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  animations: [
+    trigger('AnimationTrigger0', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'hero-icons-showcase';
+
+  availableHeroIcons: string[];
+  isSlideOver = false;
+  isSlideOverDialog = false;
+  previewIcon!: {
+    name: string;
+    class: string;
+  };
+  snippets = {
+    heroIcon: `<hero-icon 
+    name="academic-cap" 
+    styles="outline"></hero-icon>`
+  }
+
+  constructor(private renderer: Renderer2) {
+    this.availableHeroIcons = heroIconArray;
+  }
+
+  onPreviewIcon(icon: string) {
+    this.previewIcon = {
+      name: icon,
+      class: (!this.previewIcon) ? 'h-20 w-20 block' : this.previewIcon.class
+    }
+    this.toggleSlideOver();
+  }
+
+  toggleSlideOver() {
+    const delay = this.isSlideOverDialog ? 400 : 0;
+    setTimeout(() => {
+      this.isSlideOverDialog = !this.isSlideOverDialog;
+    }, delay);
+
+    this.isSlideOver = !this.isSlideOver;
+  }
+
+  updateIconClass(event: Event) {
+    const newClass = event.target as HTMLInputElement;
+    const element = document.querySelector('#preview-icon');
+
+    this.renderer.removeAttribute(element, 'class');
+    this.renderer.setAttribute(element, 'class', newClass.value);
+
+    this.previewIcon.class = newClass.value;
+  }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
+  mode: 'all',
   content: [
-    "./projects/hero-icons-showcase/src/**/*.{html,ts}"
+    "./projects/hero-icons-showcase/src/**/*.{html,ts}",
+    "./projects/hero-icons-showcase/src/index.html"
+  ],
+  safelist: [
+    {
+      pattern: /^text-/,
+      variants: ['hover'],
+    }
   ],
   important: true,
   theme: {
@@ -11,5 +19,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: []
 }
